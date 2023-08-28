9e7ab8f1322454237cb312fd1b63eee627061f71
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-08-28 23:38:57 +0100

    Update ImageCard.jsx
    Added a border for when an image is hovered over
    Clicking on an image directs the user to the page for the image
    Added comments list item

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -5,7 +5,13 @@ const ImageCard = ({ image }) => {
 
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src={image.webformatURL} alt="" className="w-full" />
+      <img
+        src={image.webformatURL}
+        alt=""
+        className="hover:border-solid hover:border-2 hover:border-purple-400 w-full"
+        onClick={() => (window.location.href = image.pageURL)}
+      />
+
       <div className="px-6 py-4">
         <div className="font-bold text-purple-500 text-xl mb-2">
           Photo by {image.user}
@@ -22,6 +28,10 @@ const ImageCard = ({ image }) => {
           <li>
             <strong>Likes: </strong>
             {image.likes}
+          </li>
+          <li>
+            <strong>Comments: </strong>
+            {image.comments}
           </li>
         </ul>
       </div>
